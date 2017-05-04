a271080e2b4c506ec96b3c7537bf0bcb1420498a
zc <zc_93@qq.com>
2017-05-04 22:40:20 +0800

    post_pop

--- vue2/src/pages/PersonalCenter.vue
+++ vue2/src/pages/PersonalCenter.vue
@@ -6,8 +6,8 @@
             <mu-text-field v-model="title" hintText="标题" type="text" icon="title"/><br/>
             <mu-text-field v-model="content" hintText="正文" multiLine :rows="10" :rowsMax="20" icon="assignment"/><br/>
             <mu-raised-button v-on:click="publish" label="Publish" class="demo-raised-button" primary/>
+            <mu-popup position="top" :overlay="false" popupClass="demo-popup-top" :open="topPopup">{{msg}}</mu-popup>
         </div>
-
     </div>
 </template>
 
@@ -18,7 +18,9 @@
             return {
                 isActive: true, hasError: true,
                 title:'',
-                content:''
+                content:'',
+                topPopup: false,
+                msg:''
             }
         },
         computed:{
@@ -33,13 +35,25 @@
                         content:this.content,
                     })
                 ).then(function (res) {
-                    console.log(res);
-                    if(res&&res.data.result===0){
-
+                    if(res&&res.data===0){
+                        this.msg='成功';
                     }else{
-
+                        this.msg=res.statusText;
                     }
+                    this.open('top');
                 }.bind(this))
+            },
+            open (position) {
+                this[position + 'Popup'] = true
+            },
+        },
+        watch: {
+            topPopup (val) {
+                if (val) {
+                    setTimeout(() => {
+                        this.topPopup = false
+                    }, 3000)
+                }
             }
         }
     }
